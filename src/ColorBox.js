8d8e88686fbb81cccc04f38b3c599030734c22e3
PianoFF <Fangfei1star@gmail.com>
2020-01-06 15:07:56 +0000

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
-
+  //Note: The style attribute 
+  // inner {} __ accepts a JavaScript object with camelCased properties rather than a CSS string;
+  // outer {} _ to render vanilla Javascript; 
   render() {
     return (
-      <div className="color-box" style={{opacity: 2}}>   //Note: The style attribute accepts a JavaScript object with camelCased properties rather than a CSS string
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: this.props.opacity}}> 
+          {this.props.opacity >=0.2 ?  <ColorBox opacity={this.props.opacity - 0.1} /> : null}
       </div>
     )
   }
 
 }
+/* with short circuit conditional: (vs. ternary above)
+  <div className="color-box" style={{opacity: this.props.opacity}}> 
+          {this.props.opacity >=0.2 && <ColorBox opacity={this.props.opacity - 0.1} />}
+  </div>
+*/
 
+/* another solution: 
+
+In order to render based on a conditional, you can write vanilla JavaScript logic directly in render() block --> 
+
+class ColorBox extends Component{
+  render(){
+    if (this.props.opacity >= 0.2){
+      return (
+        <div classname='color-box' style={{opacity: this.props.opacity}}>
+        <ColorBox opacity={this.props.opacity - 0.1} /> // 👈 rendering ColorBox within ColorBox
+        </div>
+      )
+    } else {
+      return null;
+    }
+  }
+} */
